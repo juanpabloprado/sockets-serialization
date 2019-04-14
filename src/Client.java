1c2b55781b9f22efd23db0408295f3a14d767b8b
Juan Pablo Prado <juan@juanpabloprado.com>
2019-04-13 20:59:07 -0500

    first commit

---
+++ src/Client.java
@@ -0,0 +1,25 @@
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.time.LocalDate;
+
+public class Client {
+
+	public static void main(String args[]) {
+
+		try (Socket socket = new Socket(InetAddress.getLocalHost(), 8020);
+				OutputStream o = socket.getOutputStream();
+				ObjectOutput s = new ObjectOutputStream(o)) {
+			// Serialize today's date to a output stream associated to the socket
+			s.writeObject("Today's date");
+			s.writeObject(LocalDate.now());
+			s.flush();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			System.out.println("Error during serialization");
+			System.exit(1);
+		}
+	}
+}
---
+++ src/Server.java
@@ -0,0 +1,33 @@
+import java.io.InputStream;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.time.LocalDate;
+
+public class Server {
+
+	// Create the server socket and use its stream to receive serialized objects
+	public static void main(String args[]) {
+
+		Socket socket;
+		String str;
+		LocalDate date;
+
+		try (ServerSocket server = new ServerSocket(8020)) {
+			// This will wait for a connection to be made to this socket.
+			socket = server.accept();
+			InputStream o = socket.getInputStream();
+			ObjectInput s = new ObjectInputStream(o);
+			str = (String) s.readObject();
+			date = (LocalDate) s.readObject();
+
+			System.out.println(str);
+			System.out.println(date);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			System.out.println("Error during serialization");
+			System.exit(1);
+		}
+	}
+}
